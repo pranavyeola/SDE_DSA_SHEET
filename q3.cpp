2a0fc1dc993202e5d9648047a26f6491eba4f217
Pranav Yeola <45764776+pranavyeola@users.noreply.github.com>
2020-10-26 01:35:45 +0530

    ARRAY- Q3 Count no. of trailing zeroes in factorial of a number.

---
+++ q3.cpp
@@ -0,0 +1,13 @@
+/* Count the trailing zeroes of factorial of a number*/
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int count=0;
+        for(int i=5;n/i>=1;i*=5)
+        {
+            count+=n/i;
+            
+        }
+        return count;
+    }
+};
